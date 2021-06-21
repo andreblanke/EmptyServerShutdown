67179c572d5cce60a22942ecd9ad1f357194197f
Andre Blanke <blanke.and@gmail.com>
2021-06-21 18:30:05 +0200

    Populate AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,13 +2,16 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle        ("EmptyServerShutdown")]
-[assembly: AssemblyDescription  ("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany      ("")]
+[assembly: AssemblyDescription  ("TShock plugin to shut down empty servers")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: AssemblyProduct      ("EmptyServerShutdown")]
-[assembly: AssemblyCopyright    ("Copyright ©  2021")]
-[assembly: AssemblyTrademark    ("")]
-[assembly: AssemblyCulture      ("")]
+[assembly: AssemblyCopyright    ("Copyright © Andre Blanke 2021")]
 [assembly: ComVisible           (false)]
 [assembly: Guid                 ("06054E55-7E7D-48EC-B874-5E08D88F0E0C")]
 [assembly: AssemblyVersion      ("1.0.0.0")]
